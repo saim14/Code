12e7574b21d9eaff77fbd2dc2724e63fe4710d4a
Saim Islam <saim.islam698@gmail.com>
2024-09-01 14:53:38 +0600

    Religion added

---
+++ container_types.py
@@ -0,0 +1,9 @@
+# Ordered sequences
+x = [1,2,3] # mutable
+y = (1,5,7) # immutable
+
+# Key containers, no a priori order
+xy = {"key", "value"} # dictionary
+yx = {1,4,5,6} # set
+
+px = {1: "one", 2: "Two"} # dictionary
